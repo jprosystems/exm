892eb65a4b44d7223f7ecccd9ca9f6b4aad1d92e
ARTHUR-PC\P. M. Marc-Arthur <mcarthurp1993@gmail.com>
2018-03-24 03:10:22 -0700

    Histoire de tester SourceTree

--- js/pages/ui/modals.js
+++ js/pages/ui/modals.js
@@ -2,6 +2,6 @@ $(function () {
     $('.js-modal-buttons .btn').on('click', function () {
         var color = $(this).data('color');
         $('#mdModal .modal-content').removeAttr('class').addClass('modal-content modal-col-' + color);
-        $('#mdModal').modal('show');
+        $('#mdModal').modal('show'); //to show the modal.
     });
 });
