d50969f0e09f69dd51566ca9384d3249cc50973b
KFG_JESUS\owner <rodriguejeandanielpierre@gmail.com>
2018-04-02 19:15:48 -0400

    eXmapi et dépendances

--- eXmapi/eXmapi/model/dbserveur.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type DbServer struct {
-	Host    string `json:"DB_HOST"`
-	User    string `json:"DB_USER"`
-	Pass    string `json:"DB_PASS"`
-	DbPath  string `json:"DB_PATH"`
-	DbRdbms string `json:"DB_RDBMS"`
-}
